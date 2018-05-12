fc46813420f6072bb201ca68ed77f14f5701dadc
Reebes72 <acreeves72@gmail.com>
2018-05-12 11:40:07 -0500

    Added New Exercise

---
+++ EmployeeClass411/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeClass411
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private decimal monthlySalary;
+
+        public Employee(string first, string last, decimal salary)
+        {
+            FirstName = first;
+            LastName = last;
+            MonthlySalary = salary;
+        }
+
+
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalary;
+            }
+            private set
+            {
+                if (value > 0.0m)
+                {
+                    monthlySalary = value;
+                }
+            }
+
+        }
+
+        public void GiveRaise()
+        {
+            MonthlySalary = MonthlySalary * 1.10m;
+        }
+
+        public decimal GetYearlySalary()
+        {
+            return MonthlySalary * 12;
+        }
+        
+    }
+}
---
+++ EmployeeClass411/EmployeeTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EmployeeClass411
+{
+    class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Bob", "Builder", 4000m);
+            Employee emp2 = new Employee("Dora", "Explorer", 1000m);
+
+            Console.WriteLine(
+                $"{emp1.FirstName} the {emp1.LastName} earns {emp1.MonthlySalary} for {emp1.GetYearlySalary()} a year" +
+                $"\n Let's give this employee a raise!");
+            emp1.GiveRaise();
+            Console.WriteLine(
+                $"Now {emp1.FirstName} makes {emp1.MonthlySalary} a month and {emp1.GetYearlySalary()} a year\n");
+
+            Console.WriteLine(
+                $"{emp2.FirstName} the {emp2.LastName} earns {emp2.MonthlySalary} for {emp2.GetYearlySalary()} a year" +
+                $"\n Let's give this employee a raise!");
+            emp2.GiveRaise();
+            Console.WriteLine(
+                $"Now {emp2.FirstName} makes {emp2.MonthlySalary} a month and {emp2.GetYearlySalary()} a year\n");
+        }
+    }
+}

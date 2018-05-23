2831c06d7a3b322cc0fcd08061462569958847f2
Reebes72 <acreeves72@gmail.com>
2018-05-23 14:46:58 -0500

    File storing changed. Two exercises added

---
+++ Palindromes531/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Palindromes531
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input ="";
+            while (input.Length < 5 || input.Length > 5)
+            {
+                Console.WriteLine(
+                    "Please enter a 5 digit integer.");
+                input = Console.ReadLine();
+            }
+
+            
+               int  palindrome = int.Parse(input);
+            
+
+
+
+            int temp = palindrome;
+            int temp1, temp2, temp4, temp5;
+
+            temp5 = palindrome % 10;
+            palindrome /= 10;
+            temp4 = palindrome % 10;
+            palindrome /= 100;
+            temp2 = palindrome % 10;
+            palindrome /= 10;
+            temp1 = palindrome;
+
+            if(temp1 == temp5 && temp2 == temp4)
+            {
+                Console.WriteLine(
+                    $"{temp} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine(
+                    $"{temp} is not a palindrome.");
+            }
+
+
+            
+        }
+    }
+}
---
+++ SquareOfAsterisks530/AsterisksSquare.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SquareOfAsterisks530
+{
+    class AsterisksSquare
+    {
+        static void Main(string[] args)
+        {
+            string[,] square;
+
+            Console.WriteLine(
+                "This program will create a square of asterisks of a size between 1-20 that you specify.\n" +
+                "Please enter a number between 1-20");
+
+            int number = int.Parse(Console.ReadLine());
+
+            if( number > 20)
+            {
+                number = 20;
+            }
+            if(number < 1)
+            {
+                number = 1;
+            }
+
+            square = new string[number, number];
+
+            for(int i = 0; i < square.GetLength(0); i++)
+            {
+                for(int j = 0; j < square.GetLength(1); j++)
+                {
+                    Console.Write(i == 0 || i+1 == square.GetLength(0) || j == 0 || j+1 == square.GetLength(1) ? "*" : " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
